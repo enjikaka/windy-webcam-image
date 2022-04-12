ec03d14f536c09c4f202a0041773e557e5822f00
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2022-04-12 19:15:26 +0200

    Add index

---
+++ index.ts
@@ -0,0 +1,42 @@
+import { serve } from 'https://deno.land/std@0.134.0/http/server.ts';
+
+async function respond (webcamId: string | undefined) {
+  if (!webcamId) {
+    throw new TypeError('webcamId can not be undefined');
+  }
+
+  const windyUrl = new URL(`https://api.windy.com/api/webcams/v2/list/webcam=${webcamId}`);
+
+  windyUrl.searchParams.append('show', 'webcams:location,image');
+  windyUrl.searchParams.append('key', Deno.env('API_KEY'));
+
+  const response = await fetch(windyUrl.toString());
+
+  if (!response.ok) {
+    throw new Error(String(response.status));
+  }
+
+  const json = await response.json();
+
+  return new Response(null, {
+    status: 302,
+    headers: new Headers({
+      'Location': json.result.webcams[0].image.daylight.preview
+    })
+  });
+}
+
+function handler(req: Request): Promise<Response> | Response {
+  const [, webcamId] = new URL(req.url).pathname;
+
+  try {
+    return respond(webcamId);
+  } catch (e) {
+    return new Response('message' in e ? e.message : null, {
+      status: 400
+    });
+  }
+}
+
+console.log("Listening on http://localhost:8000");
+await serve(handler);
